843fc87a157a03625331ad98c47a3608af7be0ac
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-09-13 15:17:31 -0400

    add some basic code

---
+++ main.m
@@ -0,0 +1,33 @@
+clear;  %clear the memory
+clc;    %clear console text
+
+% Inputs
+
+posA_x = 0;
+posA_y = 0;
+posA_z = 0;
+
+va_x = 0;
+va_y = 0;
+va_z = 0;
+
+Force_md = 0;
+Force_mg = 0;
+Force_p = 0;
+
+posA=[posA_x;posA_y;posA_z];
+ar = 0;
+va=[va_x;va_y;va_z];
+Forces=[Force_md Force_mg Force_p];
+
+function [pcm MI aa]=Devoir1(posA,ar,va,Forces)
+
+%1. Calculate CM of plane
+
+%2. Use CM to calculate torque (moment d'inertie) of plane
+
+%3. Calculate angular acceleration
+
+end
+
+
