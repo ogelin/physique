c2b18bdcdcbecaf38156003b3ae378c62b94b9cf
Stephanie Van Dyke <stephanie.van-dyke@polymtl.ca>
2018-09-18 16:34:28 -0400

    Fusee ->Avion lol

--- TP1/Devoir1.m
+++ TP1/Devoir1.m
@@ -1,11 +1,11 @@
 function [ pcm, MI, aa ] = Devoir1( posA,ar,va,Forces )
 
 
-    fusee = fusee(posA,ar,va,Forces);
-    pcm = fusee.getPositionCMOrigin();
-    MI = fusee.momentInertieFusee();
+    avion = avion(posA,ar,va,Forces);
+    pcm = avion.getPositionCMOrigin();
+    MI = avion.momentInertieAvion();
     
-    fprintf("momentInertieFusee : \n");
+    fprintf("momentInertieAvion : \n");
     fprintf("x = %d \n", MI(1));
     fprintf("y = %d \n", MI(2));
     fprintf("z = %d \n", MI(3));
---
+++ TP1/tests/testAvion.m
@@ -0,0 +1,29 @@
+clear all;
+clc;
+
+avion = avion([0,0,0],0,0,0);
+avion.initPartiesAvion();
+
+fprintf("\navion : \n");
+fprintf("Masse Totale : %d \n", avion.masse());
+positionavion = avion.getPositionCMOrigin();
+fprintf("Fuselage : \n");
+fprintf("x = %d \n", positionavion(1));
+fprintf("y = %d \n", positionavion(2));
+fprintf("z = %d \n", positionavion(3));
+
+
+
+momentInertieavion = avion.momentInertieavion();
+
+fprintf("momentInertieavion : \n");
+fprintf("x = %d \n", momentInertieavion(1));
+fprintf("y = %d \n", momentInertieavion(2));
+fprintf("z = %d \n", momentInertieavion(3));
+
+momentInertieavionOrigine = avion.momentInertieavionOrigine(positionavion);
+
+fprintf("momentInertieavionOrigine : \n");
+fprintf("x = %d \n", momentInertieavionOrigine(1));
+fprintf("y = %d \n", momentInertieavionOrigine(2));
+fprintf("z = %d \n", momentInertieavionOrigine(3));
