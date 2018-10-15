1fb080f81093e4f93df6a7b29bb16b3549d6691e
Titouan <titouan.labbe@polymtl.ca>
2018-10-15 13:36:42 -0400

    Ajout du rayon du ballon aux mesures de collisions de poteau dans VerifierCollision

--- Devoir2/verifierCollision.m
+++ Devoir2/verifierCollision.m
@@ -2,79 +2,80 @@
 
 function [but] = verifierCollision (qn)
 
+#La mesure 0.11 est le rayon du ballon
 
 # Il y a un but du côté gauche
 if (qn(0) < 0 && 17.5 < qn(1) < 24.820 && 0 < qn(2) < 2.44)
 but = 1;
 
 
-# Il y a un but du côté gauche
-elseif (qn(0) > 120 && 17.5 < qn(1) < 24.820 && 0 < qn(2) < 2.44)
+# Il y a un but du côté droit
+elseif (qn(0) > 120 && (17.5 + 0.11) < qn(1) < (24.820 - 0.11) && 0 < qn(2) < (2.44 - 0.11))
 but = 1;
 
 
 # Le ballon touche le sol en premier
-elseif (z <= 0)
+elseif (q(2) <= 0.11)
 but = 0;
 
 
 # Le ballon touche les montants du buts gauche
 # Poteau proche de l'origine
-elseif ( qn(0) = 0 && qn(1) = 17.5 && 0 < qn(2) < 2.44 )
+elseif ( qn(0) = 0 &&  (17.5 - 0.11) <= qn(1) <= (17.5 + 0.11) && 0 < qn(2) < (2.44 - 0.11) )
 but = -1;
 
 
 # Le ballon touche les montants du buts gauche
 # Poteau loin de l'origine
-elseif ( qn(0) = 0 && qn(1) = 24.820 && 0 < qn(2) < 2.44 )
+elseif ( qn(0) = 0 && (24.820 - 0.11) <= qn(1) <= (24.820 - 0.11) && 0 < qn(2) < (2.44 - 0.11) )
 but = -1;
 
 
 # Le ballon touche les montants du buts gauche
 # Poteau transversale
-elseif ( qn(0) = 0 && 17.5 < qn(1) < 24.820  && qn(2) = 2.44 )
+elseif ( qn(0) = 0 && (17.5 + 0.11) < qn(1) < (24.820 - 0.11)  && (2.44 - 0.11) <= qn(2) <= (2.44 + 0.11) )
 but = -1;
 
 
 # Le ballon touche les montants du buts droit
 # Poteau proche de l'origine
-elseif ( qn(0) = 120 && qn(1) = 17.5 && 0 < qn(2) < 2.44 )
+elseif ( qn(0) = 120 && (17.5 - 0.11) <= qn(1) <= (17.5 + 0.11) && 0 < qn(2) < (2.44 - 0.11))
 but = -1;
 
 
 # Le ballon touche les montants du buts droit
 # Poteau loin de l'origine
-elseif ( qn(0) = 120 && qn(1) = 24.820 && 0 < qn(2) < 2.44 )
+elseif ( qn(0) = 120 && (24.820 - 0.11) <= qn(1) <= (24.820 - 0.11) && 0 < qn(2) < (2.44 - 0.11) )
 but = -1;
 
 
 # Le ballon touche les montants du buts droit
 # Poteau transversale
-elseif ( qn(0) = 120 && 17.5 < qn(1) < 24.820  && qn(2) = 2.44 )
+elseif ( qn(0) = 120 && (17.5 + 0.11) < qn(1) < (24.820 - 0.11)  && (2.44 - 0.11) <= qn(2) <= (2.44 + 0.11) )
 but = -1;
 
 
 #Le ballon sort du terrain
-#Touché loin de l'origine
-elseif ( 0 <= qn(0) <= 120 && 190 < qn(1) && 0 < qn(2) )
+#Sortie vers le haut
+elseif ( 0 <= qn(0) <= 120 && 90 < qn(1) && 0.11 <= qn(2) )
 but = -2;
 
 
 #Le ballon sort du terrain
-#Touché loin de l'origine
-elseif ( 0 <= qn(0) <= 120 && qn(1) < 0  && 0 < qn(2) )
+#Sortie vers le bas
+elseif ( 0 <= qn(0) <= 120 && qn(1) < 0  && 0.11 <= qn(2) )
 but = -2;
 
 
 #Le ballon sort du terrain
-#Corner côté gauche
-elseif ( qn(0) < 0 && ( 0 < qn(1) < 17.5 ) || ( 24.820 < qn(1) < 90 )  && 0 <= qn(2) )
+#Sortie vers la gauche
+elseif ( qn(0) < 0 && ( 0 < qn(1) < (17.5-0.11) ) || ( (24.820+0.11) < qn(1) < 90 )  && 0.11 <= qn(2) )
 but = -2;
 
 
 #Le ballon sort du terrain
-#Corner côté droit
-elseif ( 120 < qn(0) && ( 0 < qn(1) < 17.5 ) || ( 24.820 < qn(1) < 90 )  && 0 <= qn(2) )
+#Sortie vers la droite
+elseif ( 120 < qn(0) && ( 0 < qn(1) < (17.5-0.11) ) || ( (24.820+0.11) < qn(1) < 90 )  && 0.11 <= qn(2) )
 but = -2;
 
 endif
