a44c6ca586f0f010cce01fc286b0e397ecfcffce
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-10-31 13:39:02 -0400

    rotation des axes et verification en z

--- Devoir3/verifierCollision.m
+++ Devoir3/verifierCollision.m
@@ -3,5 +3,11 @@ function collision = verifierCollision (positionCMBalle, positionCMBoite, thetaB
   %la balle et la boite selon les positions données.
   %Sinon retourne Constantes.COUP_REUSSI (=0) s'il n'y a pas de collision
   
+  positionCMBalleModif = matriceRotationY(thetaBoite)*positionCMBalle;
+  positionCMBoiteModif = matriceRotationY(thetaBoite)*positionCMBoite;
+  
+  distZ = Constantes.HAUTEUR_BOITE_m/2 + Constantes.RAYON_BALLE_m/2;
+  
+  estDansBornesZ = (positionCMBalleModif(3) <= positionCMBoiteModif(3) + distZ) & (positionCMBalleModif(3) >= positionCMBoiteModif(3) - distZ);
 
 endfunction
