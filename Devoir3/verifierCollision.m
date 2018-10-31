aef91461f3901f4b9c0e68e65bda90920e7a0bba
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-10-31 13:11:06 -0400

    debut verifier collision

---
+++ Devoir3/verifierCollision.m
@@ -0,0 +1,7 @@
+function collision = verifierCollision (positionCMBalle, positionCMBoite, thetaBoite)
+  %Retourne Constantes.COUP_REUSSI (=1) si il y a collision entre 
+  %la balle et la boite selon les positions données.
+  %Sinon retourne Constantes.COUP_REUSSI (=0) s'il n'y a pas de collision
+  
+
+endfunction
