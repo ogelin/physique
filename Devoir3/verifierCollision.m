6053008c0570144ad8a1451d4e6da5df2b6e143a
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-10-31 14:52:17 -0400

    commentaires

--- Devoir3/verifierCollision.m
+++ Devoir3/verifierCollision.m
@@ -3,14 +3,17 @@ function collision = verifierCollision (positionCMBalle, positionCMBoite, thetaB
   %la balle et la boite selon les positions données.
   %Sinon retourne Constantes.COUP_MANQUE (=0) s'il n'y a pas de collision
   
+  %Rotation du système d'axes
   positionCMBalleModif = matriceRotationY(thetaBoite)*positionCMBalle;
   positionCMBoiteModif = matriceRotationY(thetaBoite)*positionCMBoite;
   
+  %Vérification des bornes d'en haut et d'en bas du cylindre
   distZ = Constantes.HAUTEUR_BOITE_m/2 + Constantes.RAYON_BALLE_m;
   
   estDansBornesZ = (positionCMBalleModif(3) <= positionCMBoiteModif(3) + distZ) ...
                     && (positionCMBalleModif(3) >= positionCMBoiteModif(3) - distZ);
   
+  %Vérification des bornes du cercle
   estDansBornesXY = sqrt((positionCMBalleModif(1)-positionCMBoiteModif(1))^2 ...
                     + (positionCMBalleModif(2)-positionCMBoiteModif(2))^2) ...
                     <= Constantes.RAYON_BOITE_m+Constantes.RAYON_BALLE_m;
