eb84002196ebd5159ba507b98c62df902caeee3d
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-11-01 16:42:26 -0400

    correction nom var

--- Devoir3/calculImpulsion.m
+++ Devoir3/calculImpulsion.m
@@ -2,6 +2,8 @@ function [j] = calculerImpulsion(vitesseCMBalle, vitesseCMBoite, wiBoite, thetaB
   
   denominateur_j = 1/Constantes.MASSE_BALLE_kg + 1/Constantes.MASSE_BOITE_kg; 
   
+  
+  
   vr_avant = [0,0,0]; %TODO
   
   numerateur_j = -vr_avant*(1+Constantes.COEFFICIENT_RESTITUTION);
