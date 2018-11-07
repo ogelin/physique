4490ac77f20112ec5dcf470c5f7a839dc7b9e06f
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-11-07 14:31:01 -0500

    debut de correction de vect normal

--- Devoir3/trouverVecteurNormal.m
+++ Devoir3/trouverVecteurNormal.m
@@ -1,7 +1,13 @@
 function [vecteurNormal] = trouverVecteurNormal(rCMBalle, rCollision, typeCollision)
     tmp = rCMBalle - rCollision; %Pas bon!!!
     
+    if(typeCollision==Constantes.COLLISION_BASE)
     
+    elseif(typeCollision==Constantes.COLLISION_COTE)
+    
+    elseif(typeCollision==Constantes.COLLISION_ARETE)
+    
+    endif
     
     vecteurNormal = tmp / sqrt(tmp(1)^2+tmp(2)^2+tmp(3)^2); %rendre le vecteur unitaire en divisant par la norme
 endfunction
