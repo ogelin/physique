e3f757049ff7215a62ba58c0ebaed06d0eea69af
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-11-07 14:37:41 -0500

    continuer calcul de la normale

--- Devoir3/trouverVecteurNormal.m
+++ Devoir3/trouverVecteurNormal.m
@@ -1,13 +1,23 @@
-function [vecteurNormal] = trouverVecteurNormal(rCMBalle, rCollision, typeCollision)
-    tmp = rCMBalle - rCollision; %Pas bon!!!
+function [vecteurNormal] = trouverVecteurNormal(rCMBalle, rCMBoite, theta, typeCollision)
     
+    %rotation du système d'axes
+    positionCMBalleModif = matriceRotationY(thetaBoite)*transpose(rCMBalle);
+    positionCMBoiteModif = matriceRotationY(thetaBoite)*transpose(rCMBoite);
+    
+    vecteurNormalModif = [0,0,0]; %TODO
+      
     if(typeCollision==Constantes.COLLISION_BASE)
+      
     
     elseif(typeCollision==Constantes.COLLISION_COTE)
     
     elseif(typeCollision==Constantes.COLLISION_ARETE)
     
-    endif
+  endif
+  
+   %retour au système d'axes global
+    
+    vecteurNormal = matriceRotationY(-thetaBoite)*vecteurNormalModif;
     
     vecteurNormal = tmp / sqrt(tmp(1)^2+tmp(2)^2+tmp(3)^2); %rendre le vecteur unitaire en divisant par la norme
 endfunction
