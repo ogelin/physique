c655c72cc60832a1c74196446df157d6d6134a0f
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-11-07 15:27:34 -0500

    continuer de calculer les normales

--- Devoir3/trouverVecteurNormal.m
+++ Devoir3/trouverVecteurNormal.m
@@ -1,10 +1,10 @@
 function [vecteurNormalCylindre, vecteurNormalBalle] = trouverVecteurNormal(rCMBalle, rCMBoite, theta, typeCollision)
-    
-    
+      
     %rotation du système d'axes
     positionCMBalleModif = matriceRotationY(thetaBoite)*transpose(rCMBalle);
     positionCMBoiteModif = matriceRotationY(thetaBoite)*transpose(rCMBoite);
-          
+    
+ %CAS 1: La balle entre en collision avec le cercle en haut ou en bas du cylindre
     if(typeCollision==Constantes.COLLISION_BASE)
       if (rCMBalle(3)=>positionCMBoiteModif(3) + Constantes.HAUTEUR_BOITE_m/2)
         %collision avec la base circulaire de haut
@@ -15,10 +15,21 @@ function [vecteurNormalCylindre, vecteurNormalBalle] = trouverVecteurNormal(rCMB
       endif
       vecteurNormalBalle = rCMBalle-pointCollision;
       vecteurNormalCylindre = -vecteurNormalBalle;
-      
+ 
+ %CAS 2: La balle entre en collision avec le côté vertical du cylindre
     elseif(typeCollision==Constantes.COLLISION_COTE)
+    %on fait le meme calcul peut importe la direction d'arrivée de la balle
+      direction = [positionCMBalleModif(1)-positionCMBoiteModif(1),...
+                   positionCMBalleModif(2)-positionCMBoiteModif(2), ...
+                   positionCMBalleModif(3)];
+      %on ne se préoccupe pas de la longueur du vecteur puisqu'on retourne
+      % un vecteur unitaire à la fin du calcul
+      vecteurNormalBalle = direction;
+      vecteurNormalCylindre = - vecteurNormalBalle;
     
+  %CAS 3: La balle entre en collision avec l'arête entre le cercle et le côté
     elseif(typeCollision==Constantes.COLLISION_ARETE)
+    %.............TODO.............
     
   endif
   
