104f2661f9e487721b7b4f5e13e024f4a3dd8b04
Titouan <titouan.labbe@polymtl.ca>
2018-11-25 12:28:13 -0500

    Refactoring du code

--- Devoir4/Devoir4.m
+++ Devoir4/Devoir4.m
@@ -16,7 +16,7 @@ function [tps fTrain Itrain] = Devoir4(vtrainkmh, favion)
   positionTrainCourante = Constantes.POSITION_INITIALE_TRAIN;
   positionAvionCourante = Constantes.POSITION_AVION;
   
-  sonArrivee = false;
+  %sonArrivee = false;
   premier20db = false;
   
   %Intensite;frequencerayon;position centre d'onde (aka position avion); 
@@ -35,31 +35,12 @@ function [tps fTrain Itrain] = Devoir4(vtrainkmh, favion)
    
     nouvelleOnde = [160;favion;100; positionAvionCourante(1);positionAvionCourante(2);positionAvionCourante(3)];
     
-    %Ajout d'une nouvelle colonne;
+    %Ajout de la nouvelle onde dans la matrice;
     ondeSonore = [ondeSonore nouvelleOnde];
-                                                     
-    
-    dist = calculerDistanceEntreTrainEtAvion(positionTrainCourante,...
-                                             positionAvionCourante);
-    
-    intensite = calculerIntensiteSonoreSelonDistance(dist, favion)
-    %%sonArrivee
-    %%premier20db
-    %%fini 
-    
+                                                   
+    %Pour chacune de ces nouvelles ondes, on doit augmenter leur rayon et ajuster leur intensité et fréquence.
     for ondeAIncremente=1:size(ondeSonore,2)
- 
 
-      if(ondeSonore(1, ondeAIncremente) > 0 && !sonArrivee)
-         tps = t;
-         sonArrivee = true;
-      endif;
- 
-      if(ondeSonore(1, ondeAIncremente)  > 20 && !premier20db)
-         premier20db = true
-      endif
-   
-      if (sonArrivee) 
         nouvelleFrequence = EffetDoppler(positionTrainCourante, positionAvionCourante, vitesseTrain, favion);
         
         positionAvionEnreistree = [ondeSonore(4, ondeAIncremente), ...
@@ -70,42 +51,45 @@ function [tps fTrain Itrain] = Devoir4(vtrainkmh, favion)
                                                     positionAvionEnreistree);                                                
                                                     
         nouvelleIntensite = calculerIntensiteSonoreSelonDistance(distanceTrainetPositionAvionEnregistre, ...
-                              nouvelleFrequence);
+                              favion);
         
+        
+        %Ajustement de l'intensité et de la fréquence de l'onde
         ondeSonore(2,ondeAIncremente) = nouvelleFrequence;
-        
-        ondeSonore(1,ondeAIncremente) = nouvelleIntensite;
-      
-        sommeFrequenceDeltaT = sum(ondeSonore,[2,:])(2);
-      
-        sommeIntensiteDeltaT = sum(ondeSonore,[2,:])(1);
-        
-        %%if (ondeSonore(1, ondeAIncremente) > 20 && !premier20db )
-          %% fini = true;
-        %%endif; 
-      
-      endif; 
-      
+        ondeSonore(1,ondeAIncremente) = nouvelleIntensite; 
        
+      %On trouve le premier 20 db, ce qui nous permettra éventuellement de faire cesser le while. 
+       if(ondeSonore(1, ondeAIncremente)  > 20 && !premier20db)
+          premier20db = true
+       endif
+      
       
     end
-        
-       fTrain = [fTrain sommeFrequenceDeltaT];
-       Itrain = [Itrain sommeIntensiteDeltaT];
     
-     if (sommeIntensiteDeltaT < 20) 
-          fini = true; 
-        endif
-        
-        
-
-
+    
+    %Il faut maintenant trouver la somme des fréquences et des intensités percus par le train au temps deltat t
+    % et placer ces résultats dans les matrices correspondantes.
+    %J'ai un doute sur la manière dont on l'a fait.
+    sommeIntensiteDeltaT = sum(ondeSonore,[1,:])(1); 
+    sommeFrequenceDeltaT = sum(ondeSonore,[2,:])(2);
+      
+    fTrain = [fTrain sommeFrequenceDeltaT];
+    Itrain = [Itrain sommeIntensiteDeltaT];
+       
+       
+   %Parcours de la matrice des résutltats finaux, c'est à dire la somme de toutes les fréquences et instensités
+   % À chaque seconde pour vérifier quand cette intensité est devenue inférieure à 20 db.  Lorsque c'est le cas,
+   % on doit arrêter le while. 
+   for ItrainAuTemps=1:size(Itrain,1)   
+       if(Itrain(1, ItrainAuTemps)<20 && premier20db)
+          fini = true;
+       endif     
+   end
+       
     
   t = t+deltaT;  
   endwhile
   
-  %%fTrain = [ondeSonore(2,:)];
-  %%disp(fTrain)
-  %%Itrain = [ondeSonore(1,:)];
+
   
 endfunction
--- Devoir4/calculerDeplacementTrain.m
+++ Devoir4/calculerDeplacementTrain.m
@@ -1,7 +1,6 @@
 function [nouvellePositionTrain] = calculerDeplacementTrain(deltaT,...
                                                        positionTrainCourante,...
                                                        vitesseTrain)
-  
   nouvellePositionTrain = positionTrainCourante + deltaT * vitesseTrain
   
 endfunction
