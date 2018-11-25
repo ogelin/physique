23bf467075c8b1b902426a576b5101a63499fded
Titouan <titouan.labbe@polymtl.ca>
2018-11-25 13:54:12 -0500

    Remove addition of waves

--- Devoir4/Devoir4.m
+++ Devoir4/Devoir4.m
@@ -33,60 +33,29 @@ function [tps fTrain Itrain] = Devoir4(vtrainkmh, favion)
     positionAvionCourante = calculerDeplacementAvion(deltaT, ...
                                                      positionAvionCourante);
    
-    nouvelleOnde = [160;favion;100; positionAvionCourante(1);positionAvionCourante(2);positionAvionCourante(3)];
+    distanceTrainetPositionAvion = calculerDistanceEntreTrainEtAvion(positionTrainCourante, positionTrainCourante);
     
-    %Ajout de la nouvelle onde dans la matrice;
-    ondeSonore = [ondeSonore nouvelleOnde];
-                                                   
-    %Pour chacune de ces nouvelles ondes, on doit augmenter leur rayon et ajuster leur intensité et fréquence.
-    for ondeAIncremente=1:size(ondeSonore,2)
-
-        nouvelleFrequence = EffetDoppler(positionTrainCourante, positionAvionCourante, vitesseTrain, favion);
+    nouvelleFrequence = EffetDoppler(positionTrainCourante, positionAvionCourante, vitesseTrain, favion);
+    Intensite = calculerIntensiteSonoreSelonDistance(distanceTrainetPositionAvion, ...
+                            favion);
+   
+                                        
+    fTrain = [fTrain nouvelleFrequence];
+    Itrain = [Itrain Intensite];
+                                        
+           
         
-        positionAvionEnreistree = [ondeSonore(4, ondeAIncremente), ...
-                                    ondeSonore(5, ondeAIncremente), ...
-                                    ondeSonore(6, ondeAIncremente) ];
-                                    
-        distanceTrainetPositionAvionEnregistre = calculerDistanceEntreTrainEtAvion(positionTrainCourante,...
-                                                    positionAvionEnreistree);                                                
-                                                    
-        nouvelleIntensite = calculerIntensiteSonoreSelonDistance(distanceTrainetPositionAvionEnregistre, ...
-                              favion);
-        
-        
-        %Ajustement de l'intensité et de la fréquence de l'onde
-        ondeSonore(2,ondeAIncremente) = nouvelleFrequence;
-        ondeSonore(1,ondeAIncremente) = nouvelleIntensite; 
+     if(Intensite < 20 && premier20db)
+          fini = true;
+     endif                        
+                             
        
-      %On trouve le premier 20 db, ce qui nous permettra éventuellement de faire cesser le while. 
-       if(ondeSonore(1, ondeAIncremente)  > 20 && !premier20db)
+     %On trouve le premier 20 db, ce qui nous permettra éventuellement de faire cesser le while. 
+      if(Intensite < 20 && !premier20db)
           premier20db = true
        endif
-      
-      
-    end
     
-    
-    %Il faut maintenant trouver la somme des fréquences et des intensités percus par le train au temps deltat t
-    % et placer ces résultats dans les matrices correspondantes.
-    %J'ai un doute sur la manière dont on l'a fait.
-    sommeIntensiteDeltaT = sum(ondeSonore,[1,:])(1); 
-    sommeFrequenceDeltaT = sum(ondeSonore,[2,:])(2);
-      
-    fTrain = [fTrain sommeFrequenceDeltaT];
-    Itrain = [Itrain sommeIntensiteDeltaT];
-       
-       
-   %Parcours de la matrice des résutltats finaux, c'est à dire la somme de toutes les fréquences et instensités
-   % À chaque seconde pour vérifier quand cette intensité est devenue inférieure à 20 db.  Lorsque c'est le cas,
-   % on doit arrêter le while. 
-   for ItrainAuTemps=1:size(Itrain,1)   
-       if(Itrain(1, ItrainAuTemps)<20 && premier20db)
-          fini = true;
-       endif     
-   end
-       
-    
+
   t = t+deltaT;  
   endwhile
   
--- Devoir4/calculerIntensiteSonoreSelonDistance.m
+++ Devoir4/calculerIntensiteSonoreSelonDistance.m
@@ -1,6 +1,6 @@
 function intensite = calculerIntensiteSonoreSelonDistance(r, v)
-  r
-  a = 20*log(r/100)
-  b = calculerA_v(v)*(r-100)
-  intensite = 160 - a - b
+  r;
+  a = 20*log(r/100);
+  b = calculerA_v(v)*(r-100);
+  intensite = 160 - a - b;
 endfunction
